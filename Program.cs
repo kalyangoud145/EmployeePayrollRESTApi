4de5663eac1ac773d6f9ab3327a62d022fdac588
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-01 18:31:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollRestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to problem");
+        }
+    }
+}
