dcc62c071aa6563a0cf6f2182494f387ffd62c29
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-01 19:28:36 +0530

    [Kalyan] added: testcase for adding multiple entries

--- TestRestAPI.cs
+++ TestRestAPI.cs
@@ -81,5 +81,31 @@ namespace PayrollRestApiTest
             Assert.AreEqual("150000", dataResponse.salary);
             Console.WriteLine(response.Content);
         }
+        /// <summary>
+        /// UC3
+        /// Tests the add multiple entries using post operation.
+        /// </summary>
+        [TestMethod]
+        public void TestAddMultipleEntriesUsingPostOperation()
+        {
+            //adding multiple employees to table
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(new Employee { id = 7, name = "Virat Kohli", salary = "400000" });
+            employeeList.Add(new Employee { id = 8, name = "MSD", salary = "500000" });
+            foreach (Employee employee in employeeList)
+            {
+                RestRequest request = new RestRequest("/employees", Method.POST);
+                JObject jObject = new JObject();
+                jObject.Add("name", employee.name);
+                jObject.Add("salary", employee.salary);
+                request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                //Assert
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                //derserializing object for assert and checking test case
+                Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+                Assert.AreEqual(employee.name, dataResponse.name);
+            }
+        }
     }
 }
